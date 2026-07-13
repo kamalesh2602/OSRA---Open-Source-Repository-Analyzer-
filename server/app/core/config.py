cd3f4ec2932582ee12363c5791755f2310a1b30f
kamalesh2602 <kamaleshg2602@gmail.com>
2026-07-13 22:53:27 +0530

    fix(config) : suitable for cicd

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -1,17 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "OSRA API"
+    APP_VERSION: str = "1.0.0"
 
-    GITHUB_API: str
+    GITHUB_API: str = "https://api.github.com"
     GITHUB_TOKEN: str = ""
 
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
     )
-
 
 settings = Settings()
