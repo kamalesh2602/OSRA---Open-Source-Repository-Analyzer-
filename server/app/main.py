a40c0ccac4d3eea90cbc7390474955e43f3cc506
kamalesh2602 <kamaleshg2602@gmail.com>
2026-07-15 10:03:11 +0530

    added netlify to cors

--- server/app/main.py
+++ server/app/main.py
@@ -9,7 +9,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://osra-v1.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
